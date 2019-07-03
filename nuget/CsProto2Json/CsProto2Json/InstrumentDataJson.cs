604deec0278d7b1eefa230b231177dc191833de4
Aaron Krone <akrone@hach.com>
2019-07-03 13:47:23 -0600

    Generated InstrumentData JSON

--- nuget/CsProto2Json/CsProto2Json/InstrumentDataJson.cs
+++ nuget/CsProto2Json/CsProto2Json/InstrumentDataJson.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Text;
 using AutoBogus;
+using Claros.Common.Gis;
 using Claros.Instrument;
 using Claros.Instrument.Event;
+using Claros.Instrument.Measurement;
 using Newtonsoft.Json;
 
 namespace CsProto2Json
@@ -34,6 +36,43 @@ namespace CsProto2Json
                 x.calibrationStandards.ForEach(s =>
                     s.secondaryValues.AddRange(new AutoFaker<EventDataCalibrationValue>().Generate(2)));
             });
+
+            instrumentData.Events.Errors.AddRange(new AutoFaker<EventDataMetaData>().Generate(2));
+            instrumentData.Events.Infoes.AddRange(new AutoFaker<EventDataMetaData>().Generate(2));
+            instrumentData.Events.Limits.AddRange(new AutoFaker<EventDataMetaData>().Generate(2));
+            instrumentData.Events.Reminders.AddRange(new AutoFaker<EventDataMetaData>().Generate(2));
+            instrumentData.Events.Warnings.AddRange(new AutoFaker<EventDataMetaData>().Generate(2));
+            instrumentData.Events.Prognosys.AddRange(new AutoFaker<EventDataPrognosys>().Generate(2));
+
+
+            instrumentData.Measurements.AddRange(new AutoFaker<InstrumentMeasurementData>().Generate(2));
+            instrumentData.Measurements.ForEach(d=>
+            {
+                d.Gis.MultiPoint2d.Points.AddRange(new AutoFaker<Point2D>().Generate(2));
+                d.Gis.MultiPoint3d.Points.AddRange(new AutoFaker<Point3D>().Generate(2));
+                d.measurementEvent.Calibrations.AddRange(new AutoFaker<EventDataCalibration>()
+                    .RuleFor(x => x.Id, Guid.NewGuid().ToString)
+                    .RuleFor(x => x.userID, Guid.NewGuid().ToString()).Generate(2));
+                d.measurementEvent.Calibrations.ForEach(x =>
+                {
+                    x.calibrationStandards.AddRange(new AutoFaker<EventDataCalibrationStandard>().Generate(2));
+                    x.calibrationStandards.ForEach(s =>
+                        s.secondaryValues.AddRange(new AutoFaker<EventDataCalibrationValue>().Generate(2)));
+                });
+
+                d.measurementEvent.Errors.AddRange(new AutoFaker<EventDataMetaData>().Generate(2));
+                d.measurementEvent.Infoes.AddRange(new AutoFaker<EventDataMetaData>().Generate(2));
+                d.measurementEvent.Limits.AddRange(new AutoFaker<EventDataMetaData>().Generate(2));
+                d.measurementEvent.Reminders.AddRange(new AutoFaker<EventDataMetaData>().Generate(2));
+                d.measurementEvent.Warnings.AddRange(new AutoFaker<EventDataMetaData>().Generate(2));
+                d.measurementEvent.Prognosys.AddRange(new AutoFaker<EventDataPrognosys>().Generate(2));
+            });
+
+
+            instrumentData.Settings.Add(AutoFaker.Generate<string>(), AutoFaker.Generate<string>());
+            instrumentData.Settings.Add(AutoFaker.Generate<string>(), AutoFaker.Generate<string>());
+            instrumentData.States.Add(AutoFaker.Generate<string>(), AutoFaker.Generate<string>());
+            instrumentData.States.Add(AutoFaker.Generate<string>(), AutoFaker.Generate<string>());
 
 
             var result = JsonConvert.SerializeObject(instrumentData);
