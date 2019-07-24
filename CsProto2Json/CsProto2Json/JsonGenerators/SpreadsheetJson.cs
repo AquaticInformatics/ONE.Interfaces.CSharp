da9cf9f3dfd5c448653b0232cd9c305274abb8ff
Jason Watts <jwatts@hach.com>
2019-07-24 05:35:24 -0600

    move utilities out of nuget folder

---
+++ CsProto2Json/CsProto2Json/JsonGenerators/SpreadsheetJson.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoBogus;
+using Claros.Common.Computation;
+using Claros.Common.Core;
+using Claros.Common.Form;
+using Claros.Instrument.Measurement;
+using Newtonsoft.Json;
+
+namespace CsProto2Json
+{
+
+    using Claros.Operations.Spreadsheet;
+
+    public static class SpreadsheetJson
+    {
+        public static string Generate()
+        {
+            AutoFaker.Configure(builder =>
+            {
+                builder.WithLocale("en")
+                    .WithRecursiveDepth(10)
+                    .WithRepeatCount(3);
+            });
+
+            var spreadsheetFaker = new AutoFaker<Spreadsheet>()
+                .RuleFor(prop => prop.locationId, Guid.NewGuid().ToString);
+            var worksheetFaker = new AutoFaker<Worksheet>();
+
+            var spreadsheetDefinitionFaker = new AutoFaker<SpreadsheetDefinition>();
+            var spreadsheet = spreadsheetFaker.Generate();
+            var wsdFaker = new AutoFaker<WorksheetDefinition>();
+            var rowFaker = new AutoFaker<Row>();
+            var columnFaker = new AutoFaker<Column>()
+                .RuleFor(prop => prop.locationId, Guid.NewGuid().ToString());
+
+            var cellFaker = new AutoFaker<Cell>();
+
+            spreadsheet.spreadsheetDefinitions.AddRange(spreadsheetDefinitionFaker.Generate(2));
+
+            spreadsheet.Worksheets.AddRange(worksheetFaker.Generate(2));
+            spreadsheet.Worksheets.ForEach(ws =>
+            {
+                var wsDefs = wsdFaker.Generate(2);
+                wsDefs.ForEach(wsd =>
+                {
+                    wsd.Columns.AddRange(columnFaker.Generate(2));
+                    wsd.Columns.ForEach(column =>
+                    {
+                        column.Limits.AddRange(new AutoFaker<Limit>().Generate(2));
+                        column.reportableQualiferDefinitions.AddRange(new AutoFaker<ReportableQualifierDefinition>()
+                            .Generate(2));
+                    });
+                });
+
+                ws.worksheetDefinitions.AddRange(wsDefs);
+
+                var rows = rowFaker.Generate(2);
+                rows.ForEach(r =>
+                {
+                    r.Cells.AddRange(cellFaker.Generate(2));
+                    r.Cells.ForEach(cell =>
+                    {
+                        cell.cellDatas.Add(new AutoFaker<CellData>().Generate());
+                        cell.Notes.AddRange(new AutoFaker<Note>()
+                                                .RuleFor(prop => prop.Id, Guid.NewGuid().ToString)
+                                                .RuleFor(prop => prop.userId, Guid.NewGuid().ToString)
+                                                .Generate(2));
+                        cell.cellDatas.ForEach(cd =>
+                        {
+                            cd.cellDataBindings.Add(new CellDataBinding { computationBinding = new ComputationBinding { computationId = Guid.NewGuid().ToString() } });
+                            cd.cellDataBindings.Add(new CellDataBinding
+                            {
+                                fieldInstrumentMeasurementBinding = new InstrumentMeasurementFieldBinding
+                                {
+                                    Timestamp = new AutoFaker<ClarosDateTime>().Generate(),
+                                    instrumentMeasurementId = Guid.NewGuid().ToString(),
+                                    unitId = uint.MaxValue
+                                }
+                            });
+                            cd.cellDataBindings.Add(new CellDataBinding
+                            {
+                                formBinding = new FormBinding
+                                {
+                                    formFieldId = Guid.NewGuid().ToString(),
+                                    formId = Guid.NewGuid().ToString(),
+                                    userId = Guid.NewGuid().ToString()
+                                }
+                            });
+                            cd.cellDataBindings.Add(new CellDataBinding
+                            {
+                                instrumentMeasurementBinding = new InstrumentMeasurementBinding
+                                {
+                                    unitId = 1,
+                                    instrumentMeasurementId = Guid.NewGuid().ToString(),
+                                    aggregateType = AggregateType.AggregateAverage
+                                }
+                            });
+                            cd.cellDataBindings.Add(new CellDataBinding
+                            {
+                                spreadsheetBinding = new SpreadsheetBinding
+                                {
+                                    userId = Guid.NewGuid().ToString()
+                                }
+                            });
+                        });
+
+                    });
+                });
+                ws.Rows.AddRange(rows);
+            });
+
+            var result = JsonConvert.SerializeObject(spreadsheet);
+
+            return result;
+        }
+    }
+}
---
+++ ProtoBufNsCaseFix/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+
+
+namespace ProtoBufNsCaseFix
+{
+    class Program
+    {
+        /// <summary>
+        /// This tool only assists in fixing the namespace capitalization for the generated classes. Manual changes are
+        /// still needed after running this tool but it should cut down on the amount of manual effort
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args = null)
+        {
+
+            var path = "\\generated";
+            cd if(args != null)
+                
+            Console.WriteLine("Starting...");
+            var directory = Directory.GetCurrentDirectory() + path;
+
+            // Read all namespaces and collect them 
+            var namespaces = new Dictionary<string, string>();
+
+            // HACK - Need to find a better way than hard coding this path
+            var files = Directory.EnumerateFiles(directory);
+
+            // Extract the namespaces from the files and TitleCase them (ex. claros.common.example -> Claros.Common.Example)
+            TextInfo myTI = new CultureInfo("en-US", false).TextInfo;
+            foreach (var file in files)
+            {
+                ExtractNamespaces(file).ForEach(x =>namespaces.TryAdd(x, myTI.ToTitleCase(x)));
+            }
+            
+            foreach (var file in files)
+            {
+                Console.WriteLine($"File: {file}");
+                TitleCaseNamespaces(file, namespaces);
+            }
+        }
+
+        private static void TitleCaseNamespaces(string filePath, Dictionary<string, string> namespaces)
+        {
+            var fileText = File.ReadAllText(filePath);
+
+            foreach (var ns in namespaces)
+            {
+                fileText = fileText.Replace(ns.Key, ns.Value);
+            }
+
+            using (var fs = File.OpenWrite(filePath))
+            using (var sr = new StreamWriter(fs))
+            {
+                sr.Write(fileText);
+                sr.Flush();
+            }
+        }
+
+        private static List<string> ExtractNamespaces(string file)
+        {
+            var result = new List<string>();
+
+            var lines = File.ReadAllLines(file);
+
+            result = lines.Where(x => x.StartsWith("namespace")).Select(x => x.Split(" ")[1]).ToList();
+
+            // TODO - Need to handle cases where full namespaces used with types are used but not in the namespaces. The below block doesn't work as expected
+            //var globals = lines.Where(x => x.Contains("global::claros", StringComparison.OrdinalIgnoreCase)).Select(x=> x.Trim().Split(' ')[1].Replace("global::", "")).ToList();
+            //result.AddRange(globals);
+
+            return result;
+        }
+    }
+}
