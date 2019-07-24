333c8d02e251b28e807227ecb2d51715a664846f
Aaron Krone <akrone@hach.com>
2019-07-24 11:00:04 -0600

    Another commit

--- ProtoBufNsCaseFix/Program.cs
+++ ProtoBufNsCaseFix/Program.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.Net;
 using System.Linq;
-using System.Reflection.Metadata.Ecma335;
 
 
 namespace ProtoBufNsCaseFix
