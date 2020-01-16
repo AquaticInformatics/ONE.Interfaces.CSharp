a82c4e8d9300d73666f6d698f0a4d173633560b9
Kent Miller <kmiller@hach.com>
2020-01-16 10:58:24 -0700

    CLAROS-20519: Added ApiResponseHelper

---
+++ nuget/Claros.Interfaces.CSharp/Core/IErrorCode.cs
@@ -0,0 +1,9 @@
+﻿namespace Claros.Interfaces.CSharp.Core
+{
+    public interface IErrorCode
+    {
+        FacadeStatusCode StatusCode { get; }
+        string Code { get; }
+        string ErrorDetail { get; }
+    }
+}
