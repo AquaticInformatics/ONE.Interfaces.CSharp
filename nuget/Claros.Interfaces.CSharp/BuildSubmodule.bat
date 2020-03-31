e3ef0904973b9462f844981d807ac405f1c37d31
Jonathan Robertson <jonathan.robertson@hach.com>
2020-03-31 11:15:42 -0600

    removing prebuild step because I can't get it to work in Jenkins, renamed bat file to allow devs to just run locally as needed

--- nuget/Claros.Interfaces.CSharp/BuildSubmodule.bat
+++ nuget/Claros.Interfaces.CSharp/BuildSubmodule.bat
@@ -2,5 +2,8 @@
 cd ..\Claros.Interfaces.ProtocolBuffers\generators
 Powershell.exe -executionpolicy Bypass -File gen_all.ps1
 
+# Create folder
+mkdir ..\..\Claros.Interfaces.CSharp\generated
+
 # Copy to local folder
 copy ..\proto\flat\out\csharp\* ..\..\Claros.Interfaces.CSharp\generated
