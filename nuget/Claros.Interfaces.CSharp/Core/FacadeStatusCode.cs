08579d0014f8a49801a030a58d3bd906148ac0e0
Jonathan Robertson <jonathan.robertson@hach.com>
2020-07-07 15:37:46 -0600

    merged from develop

--- nuget/Claros.Interfaces.CSharp/Core/FacadeStatusCode.cs
+++ nuget/Claros.Interfaces.CSharp/Core/FacadeStatusCode.cs
@@ -8,6 +8,7 @@
         NoContent = 204,
         BadRequest = 400,
         Unauthorized = 401,
+        Forbidden = 403,
         NotFound = 404,
         MethodNotAllowed = 405,
         InternalServerError = 500,
