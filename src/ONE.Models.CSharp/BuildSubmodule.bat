7574d776c9546dd39d654b985c4661a60d68f775
Alan Liu <alan.liu@aquaticinformatics.com>
2022-12-06 14:06:07 -0800

    AQI-3933 Added proto generation clean up which will get rid of file rename or data type spliting compiling errors due to previous generated garbage proto and cs files.

--- src/ONE.Models.CSharp/BuildSubmodule.bat
+++ src/ONE.Models.CSharp/BuildSubmodule.bat
@@ -1,3 +1,8 @@
+# Clean up temporary flat proto files and previous generted cs files for a clean generation
+IF EXIST "..\ONE.Interfaces.ProtocolBuffers\proto\flat" rd /q /s "..\ONE.Interfaces.ProtocolBuffers\proto\flat"
+IF EXIST "generated" rd /q /s "generated"
+
+
 # Generate all the C# classes from proto files
 cd ..\ONE.Interfaces.ProtocolBuffers\generators
 Powershell.exe -executionpolicy Bypass -File gen_all.ps1
