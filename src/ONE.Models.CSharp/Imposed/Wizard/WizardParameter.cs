8a26e7bb148c9e45b0953886f97302f39a5a5e18
Steve Wood <swood@hach.com>
2022-12-07 14:47:21 -0700

    AQI-4028:  Fixed bugs found in ONE Library Excel Plugin

---
+++ src/ONE.Models.CSharp/Imposed/Wizard/WizardParameter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace ONE.Models.CSharp.Imposed.Wizard
+{
+    public class WizardParameter
+    {
+        // Is this a default in the wizard
+        public bool d { get; set; }
+        // Worksheet Type
+        public EnumWorksheet t { get; set; }
+        //Sampling Statistic
+        public EnumSamplingStatistic s { get; set; }
+        // List of Agency Codes
+        // Example Wizard Code: "w", "INF_FLOW"
+        public Dictionary<string, string> ac { get; set; }
+        // Optional Formula              
+        public string f { get; set; }
+
+    }
+}
