6450b8e819c689f44cb3354959ce987d3e73648f
Alan Liu <alan.liu@aquaticinformatics.com>
2024-01-12 11:50:21 -0800

    AQI-6404 Align EnumRelativeDateRange definition to be the same to frontend enum RelativeDateRange.

--- src/ONE.Models.CSharp/Imposed/Enums/EnumRelativeDateRange.cs
+++ src/ONE.Models.CSharp/Imposed/Enums/EnumRelativeDateRange.cs
@@ -9,7 +9,17 @@
         LastYear = 4,
         LastQuarter = 5,
         LastSemiAnnual = 6,
-        Last12Months = 7
-
+        Last12Months = 7,
+        Today = 8,
+        Yesterday = 9,
+        WeekToDate = 10,
+        LastWeek = 11,
+        Last30Days = 12,
+        QuarterToDate = 13,
+        ThisWeek = 14,
+        Last14Days = 15,
+        Last60Days = 16,
+        Last90Days = 17,
+        ThisMonth = 18
     }
 }
